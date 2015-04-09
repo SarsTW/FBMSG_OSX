725aefc5c14ee6b9eb733db089a7379f42056be9
Sars <github@sars.tw>
2015-04-09 15:29:35 +0800

    Initial Commit

---
+++ fbmsg/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fbmsg
+//
+//  Created by Sars on 4/9/15.
+//  Copyright (c) 2015 SC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ fbmsg/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  fbmsg
+//
+//  Created by Sars on 4/9/15.
+//  Copyright (c) 2015 SC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
